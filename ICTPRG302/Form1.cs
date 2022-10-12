13c8dbe6a2176da0c4275c9ab2c219beab9a5ce6
Nathan Morphett <100103354+nmorphet@users.noreply.github.com>
2022-10-12 15:00:48 +1030

    Add files via upload

--- ICTPRG302/Form1.cs
+++ ICTPRG302/Form1.cs
@@ -11,16 +11,22 @@ namespace Percentage_Calculator
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //will only function if allocated cases is not 0
             if (allocatedCases.Text != "0")
             {
+                //calculates the percentage of closed/allocated cases
                 decimal calculate_percentage = ((Convert.ToDecimal(closedCases.Text)) / (Convert.ToDecimal(allocatedCases.Text)) * 100);
+                //ensures that closed cases can't exceed allocated cases and that the username box has a name
                 if (calculate_percentage <= 100 && userbox.Text != "")
                 {
+                    //prints the calculated result to the display box, formatted to two decimals with a % 
                     percentageRes.Text = calculate_percentage.ToString("0.00") + "%";
+                    //creating variables to set the path and file name
                     string directory = @"C:\Users\Nathan.Morphett\Desktop\Visual_Studio\Percentage_Calculator";
                     string filename = String.Format("{0:dd-MM-yyyy}.txt", DateTime.Now);
                     string path = Path.Combine(directory, filename);
-
+                    //saving the text output to username box and result box to a text file, appending any entries
+                    //done on the same day
                     using (StreamWriter SW = File.AppendText(path))
                     {
                         SW.WriteLine(userbox.Text);
@@ -28,6 +34,16 @@ namespace Percentage_Calculator
                         SW.Close();
                     }
                 }
+                //prints error if invalid inputs
+                else
+                {
+                    MessageBox.Show("Please check inputs", "ERROR 451", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            //prints error if invalid inputs
+            else
+            {
+                MessageBox.Show("Please check inputs", "ERROR 451", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
